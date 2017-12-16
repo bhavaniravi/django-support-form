cc9754b4e4690dd192d61f8c9a01240d49b5ee2e
bhavani <bhava0895@gmail.com>
2017-12-16 20:03:04 +0530

    add SAVE_SUPPORT_QUERY field to configure wetr to save support query in DB

--- supportform/settings.py
+++ supportform/settings.py
@@ -3,3 +3,4 @@ from django.conf import settings
 SUPPORT_EMAIL = getattr(settings, 'SUPPORT_EMAIL', settings.DEFAULT_FROM_EMAIL)
 SUPPORT_EMAIL_SUBJECT = getattr(settings, 'SUPPORT_EMAIL_SUBJECT', 'Support Message')
 SUPPORT_WAIT_SEND = getattr(settings, 'SUPPORT_WAIT_SEND', True)
+SAVE_SUPPORT_QUERY = getattr(settings, 'SAVE_SUPPORT_QUERY', True)
